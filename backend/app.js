98406ce496c391cd363cedd96f2de7211640ba12
Leema02 <lemarizeq@gmail.com>
2025-04-28 11:09:38 +0300

    Added upload images contr and routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ require('dotenv').config();            // Load .env file
 const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,12 +17,18 @@ connectDB();
 
 const userRoutes = require('./routes/userRoutes');
 const truckRoutes = require('./routes/truckRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/trucks', truckRoutes);
+app.use('/api/upload', uploadRoutes);
+
+
+// Make uploads folder public
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Start the server
-
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
