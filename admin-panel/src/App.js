410c9c19445b6548950c69d25985c4cdfa7fb618
marahhanini <marahhanini2@gmail.com>
2025-05-29 02:06:45 +0300

    booking page is ready and some features in dashboard

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -4,6 +4,8 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./screens/login";
 import Home from "./screens/home";
 import UsersPage from "./screens/UsersPage";
+import BookingsCalendarPage from "./components/BookingsCalendarPage";
+import TrucksPage from "./screens/TrucksPage";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/admin/users" element={<UsersPage />} />
+        <Route path="/admin/bookings" element={<BookingsCalendarPage />} />
+        <Route path="/admin/trucks" element={<TrucksPage />} />
       </Routes>
     </Router>
   );
